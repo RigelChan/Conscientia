2a922e711ae9dfe8e254caf7cac3fa6b21517379
Rigel <polarismapping@gmail.com>
2021-02-25 21:53:10 +0000

    Work for Thursday (aaaa help me)

--- scene.py
+++ scene.py
@@ -29,12 +29,12 @@ class MainMenu(Scene):
         self.settings_button = Button(self.constants.screen_width/2-150, 400, 300, 100)
         self.exit_button = Button(self.constants.screen_width/2-150, 550, 300, 100)
     
-    def draw(self, dt):
+    def draw(self):
         background_image = pygame.image.load(self.graphics.main_menu_image)
         self.screen.blit(background_image, (self.variables.mm_bg_pos,0))
         self.screen.blit(background_image, (self.variables.mm_bg_pos + 1280,0))
 
-        self.variables.mm_bg_pos -= self.constants.mm_bg_speed * dt
+        self.variables.mm_bg_pos -= self.constants.mm_bg_speed # TODO ADD DELTA TIME.
         if self.variables.mm_bg_pos <= -1280:
             self.variables.mm_bg_pos = 0
 
@@ -67,28 +67,35 @@ class MainMenu(Scene):
 
     def mouse_menu_movement(self, event):
         pos = pygame.mouse.get_pos()
-        if self.variables.inMenu:
-            if self.play_button.isOver(pos):
-                self.variables.overPlay = True
-            else:
-                self.variables.overPlay = False
-            if self.settings_button.isOver(pos):
-                self.variables.overSettings = True
-            else:
-                self.variables.overSettings = False
-            if self.exit_button.isOver(pos):
-                self.variables.overExit = True
-            else:
-                self.variables.overExit = False
+        if self.play_button.isOver(pos):
+            self.variables.overPlay = True
+        else:
+            self.variables.overPlay = False
+        if self.settings_button.isOver(pos):
+            self.variables.overSettings = True
+        else:
+            self.variables.overSettings = False
+        if self.exit_button.isOver(pos):
+            self.variables.overExit = True
+        else:
+            self.variables.overExit = False
 
-    def mouse_menu_input(self, event):
+    def mouse_menu_input(self, event, sm):
         self.variables.clicking = True
-        if self.variables.inMenu:
-            if self.variables.overPlay:
-                self.variables.inMenu = False
-                self.variables.inGame = True
-            if self.variables.overExit:
-                sys.exit(0)
+        if self.variables.overPlay:
+            sm.push(TestLevel(self))
+        if self.variables.overExit:
+            sys.exit(0)
+
+    def input(self, event, sm):
+        self.mouse_menu_movement(event)
+        self.mouse_menu_input(event, sm)
+
+    def exit(self):
+        print("Leaving main menu.")
+
+    def enter(self):
+        print("Entering main menu.")
 
 class TestLevel(Scene): 
     def __init__(self, game):
@@ -99,14 +106,45 @@ class TestLevel(Scene):
         self.screen_rect = game.screen.get_rect()
         self.player = Player()
         
-
     def draw(self):
         temp_bg = pygame.image.load(self.graphics.test_bg).convert_alpha()
         self.screen.blit(temp_bg, (0, 0))
         self.player.draw(self.screen)
 
-    def update(self, dt):
-        self.player.update(dt)
+    def update(self): # TODO RE-ADD DELTA TIME.
+        self.player.update()
+
+    def player_key_down(self, event):
+        if event.key == pygame.K_w:
+            self.player.moving_up = True
+        elif event.key == pygame.K_s:
+            self.player.moving_down = True
+        elif event.key == pygame.K_d:
+            self.player.moving_right = True
+        elif event.key == pygame.K_a:
+            self.player.moving_left = True
+        elif event.key == pygame.K_g:
+            print("unused key")
+
+    def player_key_up(self, event):
+        if event.key == pygame.K_w:
+            self.player.moving_up = False
+        elif event.key == pygame.K_s:
+            self.player.moving_down = False
+        elif event.key == pygame.K_d:
+            self.player.moving_right = False
+        elif event.key == pygame.K_a:
+            self.player.moving_left = False
+    
+    def input(self, event):
+        self.player_key_up(event)
+        self.player_key_down(event)
+
+    def exit(self):
+        print("Leaving test level.")
+
+    def enter(self):
+        print("Entering test level.")
 
 class TransitionScene(Scene):
     def __init__(self, game, fromScene, toScene):
@@ -138,18 +176,40 @@ class FadeTransitionScene(TransitionScene):
 class SceneManager:
     def __init__(self):
         self.scenes = []
+
     def input(self):
         if len(self.scenes) > 0:
             self.scenes[-1].input()
+
     def update(self):
         if len(self.scenes) > 0:
             self.scenes[-1].update()
+
     def draw(self):
         if len(self.scenes) > 0:
             self.scenes[-1].draw()
-    def pop(self, scene):
-        self.scenes.pop(scene)
+
+    def sceneExit(self):
+        if len(self.scenes) > 0:
+            self.scenes[-1].exit()
+
+    def sceneEnter(self):
+        if len(self.scenes) > 0:
+            self.scenes[-1].enter()
+
+    def pop(self):
+        self.sceneExit()
+        self.scenes.pop()
+        self.sceneEnter()
+
     def push(self, scene):
+        self.sceneExit()
         self.scenes.append(scene)
+        self.sceneEnter()
+        print(self.scenes)
+
     def set(self, scene):
-        self.scenes = [scene]
+        while len(self.scenes) > 0:
+            self.pop()
+        for s in scenes:
+            self.push(s)
